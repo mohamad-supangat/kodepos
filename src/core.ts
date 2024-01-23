48614dc15b7a6abec9460efa8633cb2d0453c246
Mohamad Supangat <hantamkoding@gmail.com>
2024-01-23 11:21:20 +0700

    remove direktorikodepos.org from providers

--- src/core.ts
+++ src/core.ts
@@ -5,7 +5,6 @@ import type { FastifyInstance, FastifyPluginOptions } from 'fastify'
 
 const load = async (app: FastifyInstance, _: FastifyPluginOptions) => {
   const providers = new SequentialRoundRobin<ProviderList>([
-    { hostname: 'direktorikodepos.org', segment: 'wilayah' },
     { hostname: 'nomorkodepos.com', segment: 'daerah' },
   ])
 
